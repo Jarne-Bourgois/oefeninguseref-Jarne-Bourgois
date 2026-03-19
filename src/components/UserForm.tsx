55d526c3de13453745aa45988708aa5416185d25
jarnebourgois <Jarne.Bourgois@student.tsaam.be>
2026-03-19 11:01:56 +0100

    commit 1

--- src/components/UserForm.tsx
+++ src/components/UserForm.tsx
@@ -1,15 +1,17 @@
 export default function UserForm() {
+  // hallo
 
-
-    return (<>
-        <form>
-            <input type="text" id="firstName" />
-            <input type="text" id="lastName" />
-            <input type="text" id="age" />
-            <select name="typeUser" id="typeUser">
-                {/* type medewerkers */}
-            </select>
-            <button type="submit">Registreer</button>
-        </form>
-    </>)
+  return (
+    <>
+      <form>
+        <input type="text" id="firstName" />
+        <input type="text" id="lastName" />
+        <input type="text" id="age" />
+        <select name="typeUser" id="typeUser">
+          {/* type medewerkers */}
+        </select>
+        <button type="submit">Registreer</button>
+      </form>
+    </>
+  );
 }
